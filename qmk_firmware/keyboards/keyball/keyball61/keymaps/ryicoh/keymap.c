77226768484414447b86a9a5de933596dea7c061
ryicoh <ryicoh@gmail.com>
2024-04-16 15:06:33 +0900

    Enable auto mouse layer in keyboard post init

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/ryicoh/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/ryicoh/keymap.c
@@ -20,25 +20,31 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "quantum.h"
 
+enum keymap_layers {
+    _BASE,
+    _MOUSE,
+    _SETTING,
+};
+
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [0] = LAYOUT_universal(
+  [_BASE] = LAYOUT_universal(
     KC_1     , KC_2     , KC_3     , KC_4     , KC_5     , KC_6     ,                                  KC_7     , KC_8     , KC_9     , KC_0     , KC_MINS  , KC_EQL   , 
     KC_TAB   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                  KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_LBRC  ,
     KC_LCTL  , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                  KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , KC_QUOT  ,
     KC_LSFT  , KC_GRV   , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B ,                   KC_B  , KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_BSLS  ,
-    KC_LNG2  , KC_LNG1  , KC_LOPT  , KC_LOPT  , KC_LCMD  , KC_SPC   , MO(1),                 KC_RBRC , KC_ENT   , _______  , _______  , _______  , KC_RBRC  , KC_BSLS
+    KC_LNG2  , KC_LNG1  , KC_LOPT  , KC_LOPT  , KC_LCMD  , KC_SPC   , MO(_MOUSE),              KC_RBRC , KC_ENT   , _______  , _______  , _______  , KC_RBRC  , KC_BSLS
   ),
 
-  [1] = LAYOUT_universal(
+  [_MOUSE] = LAYOUT_universal(
     KC_F1    , KC_F2    , KC_F3    , KC_F4    , KC_F5    , KC_F6    ,                                  KC_F7    , KC_F8    , KC_F9    , KC_F10   , _______  , KC_BSPC  ,
     KC_ESC   , _______  , _______  , KC_UP    , _______  , _______  ,                                  _______  , KC_BTN4  , KC_BTN3  , KC_BTN5  , _______  , KC_RBRC  ,
     KC_LCTL  , _______  , KC_LEFT  , KC_DOWN  , KC_RGHT  , _______  ,                                  _______  , KC_BTN1  , SCRL_MO  , KC_BTN2  , _______  , KC_BSLS  ,
     KC_LCTL  , _______  , _______  , _______  , _______  , _______  , _______  ,            _______  , _______  , _______  , _______  , _______  , _______  , _______  ,
-    _______  , _______  , _______  , _______  , _______  , _______  , _______  ,               MO(2) , _______  , _______  , _______  , _______  , _______  , _______
+    _______  , _______  , _______  , _______  , _______  , _______  , _______  ,        MO(_SETTING) , _______  , _______  , _______  , _______  , _______  , _______
   ),
 
-  [2] = LAYOUT_universal(
+  [_SETTING] = LAYOUT_universal(
     _______  , _______  , _______  , _______  , _______  , _______  ,                                  _______  , _______  , _______  , _______  , _______  , _______  ,
     _______  , _______  , _______  , _______  , _______  , _______  ,                                  _______  , _______  , _______  , _______  , _______  , _______  ,
     _______  , _______  , _______  , _______  , _______  , _______  ,                                  _______  , _______  , _______  , _______  , _______  , _______  ,
@@ -50,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 void keyboard_post_init_user() {
     // Enable auto mouse
-    set_auto_mouse_layer(1);
+    set_auto_mouse_layer(_MOUSE);
     set_auto_mouse_timeout(700);
     set_auto_mouse_enable(true);
 
