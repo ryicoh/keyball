b023ee8bef595c745430b71de3b39e9832c3384e
ryicoh <ryicoh@gmail.com>
2024-07-28 15:17:49 +0900

    refactor(keymap): adjust keymap layout

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/ryicoh/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/ryicoh/keymap.c
@@ -33,8 +33,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_1     , KC_2     , KC_3     , KC_4     , KC_5     , KC_6     ,                                  KC_7     , KC_8     , KC_9     , KC_0     , KC_MINS  , KC_EQL   , 
     KC_TAB   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                  KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_LBRC  ,
     KC_LCTL  , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                  KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , KC_QUOT  ,
-    KC_LSFT  , KC_GRV   , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B ,                   KC_B  , KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_BTN1  ,
-    KC_ESC   , KC_LNG2  , KC_LNG1  , KC_LOPT  , KC_LCMD  , KC_SPC   , KC_BTN1 ,              MO(_FN) , KC_ENT   , _______  , _______  , _______  , KC_RBRC  , KC_BSLS  ,
+    KC_LSFT  , KC_GRV   , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B ,                   KC_B  , KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , SCRL_MO  ,
+    KC_ESC   , KC_LNG2  , KC_LNG1  , KC_LOPT  , KC_LCMD  , KC_SPC   , KC_BTN1 ,              MO(_FN) , KC_ENT   , _______  , _______  , _______  , KC_RBRC  , KC_BSLS
   ),
   [_FN] = LAYOUT_universal(
     KC_F1    , KC_F2    , KC_F3    , KC_F4    , KC_F5    , KC_F6    ,                                  KC_F7    , KC_F8    , KC_F9    , KC_F10   , _______  , MO(_SET) ,
@@ -69,7 +69,7 @@ void keyboard_post_init_user() {
     // Enable auto mouse
     // set_auto_mouse_layer(_MO);
     // set_auto_mouse_timeout(700);
-    set_auto_mouse_enable(true);
+    // set_auto_mouse_enable(true);
 
     set_cpi(base_cpi);
     keyball_set_scroll_div(6);
