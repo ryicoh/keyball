208f9358444476d297e8a5f61217206346626b84
ryicoh <ryicoh@gmail.com>
2024-08-14 10:55:29 +0900

    feat: add new keymap layout for ryicoh keyboard

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/ryicoh/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/ryicoh/keymap.c
@@ -30,18 +30,18 @@ enum keymap_layers {
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BS] = LAYOUT_universal(
-    KC_ESC   , KC_1     , KC_2     , KC_3     , KC_4     , KC_5     , KC_6     ,                                  KC_7     , KC_8     , KC_9     , KC_0     , KC_MINS  ,
-    KC_TAB   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                  KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_EQL   ,
+    KC_1     , KC_2     , KC_3     , KC_4     , KC_5     , KC_6     ,                                  KC_7     , KC_8     , KC_9     , KC_0     , KC_MINS  , KC_EQL   ,
+    KC_TAB   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                  KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_LBRC  ,
     KC_LCTL  , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                  KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , KC_QUOT  ,
     KC_LSFT  , KC_GRV   , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B ,                   KC_B  , KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , MO(_FN)  ,
-    KC_BSPC  , KC_LNG2  , KC_LNG1  , KC_LOPT  , KC_LCMD  , KC_SPC   , KC_BTN1 ,             KC_BSLS  , KC_ENT   , _______  , _______  , _______  , KC_LBRC  , KC_RBRC
+    KC_ESC   , KC_LNG4  , KC_LNG1  , KC_LOPT  , KC_LCMD  , KC_SPC   , KC_BTN1 ,             KC_BSPC  , KC_ENT   , _______  , _______  , _______  , KC_RBRC  , KC_BSLS
   ),
   [_FN] = LAYOUT_universal(
-    _______  , KC_F1    , KC_F2    , KC_F3    , KC_F4    , KC_F5    , KC_F6    ,                                  KC_F7    , KC_F8    , KC_F9    , KC_F10   , MO(_SET) ,
+    KC_F1    , KC_F2    , KC_F3    , KC_F4    , KC_F5    , KC_F6    ,                                  KC_F7    , KC_F8    , KC_F9    , KC_F10   , KC_F11   , KC_F12   ,
     _______  , _______  , _______  , KC_UP    , _______  , _______  ,                                  KC_BTN3  , KC_BTN4  , KC_BTN2  , KC_BTN5  , _______  , _______  ,
     _______  , _______  , KC_LEFT  , KC_DOWN  , KC_RGHT  , _______  ,                                  _______  , KC_BTN1  , SCRL_MO  , _______  , _______  , _______  ,
     _______  , _______  , _______  , _______  , _______  , _______  , _______  ,            _______  , _______  , _______  , _______  , _______  , _______  , _______  ,
-    _______  , _______  , _______  , _______  , _______  , _______  , _______  ,            _______  , _______  , _______  , _______  , _______  , _______  , _______
+    _______  , _______  , _______  , _______  , _______  , _______  , _______  ,            _______  , _______  , _______  , _______  , _______  , _______  , MO(_SET)
   ),
   [_SET] = LAYOUT_universal(
     _______  , _______  , _______  , _______  , _______  , _______  ,                                  _______  , _______  , _______  , _______  , _______  , _______  ,
